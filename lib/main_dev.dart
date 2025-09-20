c16a0500697943f3d63ccef2028c294162c67e78
asnakeassefa <asnakeassefa28@gmail.com>
2025-09-20 12:55:52 +0300

    feat: setup dependancy injection

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,12 +1,15 @@
+import 'package:dispatcher/core/di/injection.dart';
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
 import 'app.dart';
 import 'core/flavor/flavor.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   const AppFlavor flavor = AppFlavor.development;
   const String baseUrl = 'https://api.dev.example.com';
 
   FlavorConfig.initialize(flavor: flavor, name: 'DEV', baseUrl: baseUrl);
-
+  await configureInjection(flavor.name);
   runApp(const MyApp());
 }
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,13 +1,16 @@
+import 'package:dispatcher/core/di/injection.dart';
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
 import 'app.dart';
 import 'core/flavor/flavor.dart';
 
-void main() {
+void main() async {
   const AppFlavor flavor = AppFlavor.production;
+  WidgetsFlutterBinding.ensureInitialized();
   const String baseUrl = 'https://api.example.com';
 
   FlavorConfig.initialize(flavor: flavor, name: 'PROD', baseUrl: baseUrl);
-
+  await configureInjection(flavor.name);
   runApp(const MyApp());
 }
 
